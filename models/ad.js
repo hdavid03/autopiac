8e9416a1e07427b9094deb10523b2b138954b436
Hajdu David <hdavid998@gmail.com>
2021-11-12 23:40:03 +0100

    models added

---
+++ models/ad.js
@@ -0,0 +1,15 @@
+const Schema = require('mongoose').Schema;
+const db = require('../config/database');
+const Ad = db.model('Ad', {
+	title: String,
+	brand: String,
+	state: String,
+	price: Number,
+	description: String
+	_user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+module.exports = Ad;
+
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const Schema = require('mongoose').Schema;
+const db = require('../config/database');
+const User = db.model('User', {
+    fname: String,
+    lname: String,
+    email: String,
+    passwd: String,
+    city: String,
+    phone: String
+});
+module.exports = User;
+
